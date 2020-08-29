b47290675e1812b2b374d84392dd0455b9eec8f7
barbichola0899 <barbichola0899@users.noreply.github.com>
2020-08-28 23:49:41 -0300

    'update'

--- AA02/ex3.py
+++ AA02/ex3.py
@@ -16,7 +16,7 @@ def prop_de_erro(x1,errox1,x2,errox2,op):
     return erro
 
 def senoexponencial(x,truncamento):
-    #A EXPLICÃO DESSE ALGORITMO SE ENCONTRA NO PDF SENOXEX.PDF
+    #A nLICÃO DESSE ALGORITMO SE ENCONTRA NO PDF SENOXEX.PDF
     # float x = valor que será usado para calcular e^x
     #truncamento = qual potência será usada para o truncamentp
     if not x > 0 and x <=1:
@@ -24,16 +24,16 @@ def senoexponencial(x,truncamento):
 
     else:
         y=0        
-        for exp in  list(filter(lambda x: x if x%4!=0 else None,range(1,truncamento,1))): #os expoentes utilizados são 1,2,3,5,6,7,9,10,11....
-            sinal = (-1) **(exp//4) #o sinal das parcelas de sin(x)*e^x seguem um padrão, onde a cada ciclo de 3 numeros,o sinal muda
-            deno = 0 #usada para calcular o denominador da parcela
-            n = exp//2 #usada para a iteração
-            par = 1- exp%2 #1 se exp é par, 0 se exp é ímpar
-            for exp in range((n+1-par)):#itera-se as parcelas de seno(x) (e e(x) indiretamente),por expoente, indo até n-1, se ímpar, e até n
-                sinal_parcela = (-1)**exp #calcula-se o sinal da parcela. + para expoentes 
-                deno+= sinal_parcela/(math.factorial(2*exp+1)*math.factorial(2*(n-exp)-int(par))) #esse cálculo se encontra explicitado no arquivo em senxex.pdf
-            
-            y += sinal*math.pow(x,exp)*deno
+        for n in  list(filter(lambda x: x if x%4!=0 else None,range(1,truncamento,1))): #os expoentes utilizados são 1,2,3,5,6,7,9,10,11....
+            sinal = (-1) **(n//4) #o sinal das parcelas de sin(x)*e^x seguem um padrão, onde a cada ciclo de 3 numeros,o sinal muda
+            cn = 0 #usada para calcular o denominador da parcela
+            i = 1
+            while i <= n:#itera-se as parcelas de seno(x) (e e(x) indiretamente), começando do i = 1(menor noente do sen(x))
+                sinal_parcela = (-1)**(i//2) #calcula-se o sinal da parcela. + para expoentes 1,5,9... e - para expoentes 3,7,11... 
+                cn+= sinal_parcela/(math.factorial(i)*math.factorial(n-i)) #esse cálculo se encontra nlicitado no arquivo em senxex.pdf
+                i+=2 #i só pode ser ímpar
+            #depois de calcular o denominador, o resultado é usado pra calcular sinal*x^n/denominador
+            y += sinal*math.pow(x,n)*cn
 
         return y
 
@@ -44,11 +44,11 @@ if __name__ == "__main__":
     aprox = senoexponencial(x,truncamento)
     seno = seno(x,truncamento)
     erro_seno = math.sin(x)-seno
-    exp = exponencial(x,truncamento)
-    erro_exp = (math.e**x)-exp
-    erro_estimado = prop_de_erro(seno,erro_seno,exp,erro_exp,'*')
+    n = exponencial(x,truncamento)
+    erro_n = (math.e**x)-n
+    erro_estimado = prop_de_erro(seno,erro_seno,n,erro_n,'*')
     erro_real = real-aprox
     print('f(x) = %f'%aprox)
-    print('g(x) = %f'%(seno*exp))
+    print('g(x) = %f'%(seno*n))
     print('Erro estimado = %.16f'%erro_estimado)
     print('Erro real = %.16f'%erro_real)
